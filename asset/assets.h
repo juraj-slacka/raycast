28b036ca9d692f953d4ef0b53aa6175672b98197
juraj-slacka <naryanko@gmail.com>
2025-08-16 00:30:29 +0200

    Create assets.h

---
+++ asset/assets.h
@@ -0,0 +1,18 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include "textures.h"  // Textures
+#include "sprites.h"   // Sprites
+#include "hud.h"       // HUDs like pistol sprite
+
+
+// Here is textures table (to-do)
+
+
+// Here is sprites table (to-do)
+
+
+// Here is HUD table (to-do)
+
+
+#endif
